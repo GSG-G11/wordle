20b4c0f2f66f62317ba4c9dd2dbb40730410a19d
israaHamdi <israaaburayya2012@gmail.com>
2022-04-20 09:21:21 +0200

    about-page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,60 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page</p>
+    <div className="about">
+      <h1>HOW TO PLAY</h1>
+      <p>
+        Guess the WORDLE in six tries.
+        Each guess must be a valid length-letter word. Hit the enter button to submit.
+        After each guess, the color of the tiles will
+        change to show how close your guess was to the word.
+      </p>
+      <hr />
+      <div>
+        <h2>Examples</h2>
+        <div className="ex-true">
+          <div className="ex-true-word">
+            <span style={{ background: '#6AAA64' }}>W</span>
+            <span>E</span>
+            <span>A</span>
+            <span>R</span>
+            <span>Y</span>
+          </div>
+          <p>
+            The letter
+            <b> W </b>
+            is in the word and in the correct spot.
+          </p>
+        </div>
+        <div className="ex-true">
+          <div className="ex-true-word">
+            <span>P</span>
+            <span style={{ background: '#C9B458' }}>I</span>
+            <span>L</span>
+            <span>L</span>
+            <span>S</span>
+          </div>
+          <p>
+            The letter
+            <b> I </b>
+            is in the word but in the wrong spot.
+          </p>
+        </div>
+        <div className="ex-true">
+          <div className="ex-true-word">
+            <span>V</span>
+            <span>A</span>
+            <span>G</span>
+            <span style={{ background: '#787C7E' }}>U</span>
+            <span>E</span>
+          </div>
+          <p>
+            The letter
+            <b> U </b>
+            is not in the word in any spot
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
